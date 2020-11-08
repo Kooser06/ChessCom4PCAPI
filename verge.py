069b5fe6108c28133648c5af6de9d367391c43ea
Kooser06 <46644293+Kooser06@users.noreply.github.com>
2020-11-08 03:22:03 -0500

    Update verge.py

--- verge.py
+++ verge.py
@@ -5,3 +5,18 @@ import math
 
 from itertools import count
 from collections import namedtuple
+
+class Position(namedtuple('Position', 'board score turn pieces ep')):
+    def moves(self):
+        for piece in self.pieces:
+            if piece[0] != self.turn: continue
+            for location in directions[piece[1]]:
+                for key in count(piece[2] + location, location):
+                    if key not in valid_keys: break
+                    elif self.board[key] == 0: captured = 0
+                    elif self.board[key][0] == self.turn or self.board[key][0] == opposite_colors[self.turn]: break
+                    else: captured = 1
+                    if piece[1] in (0, 1, 2, 3) and location in (-10, -20, 10, 20, 1, -1, 2, -2) and captured == 1: break 
+                    if piece[1] in (0, 1, 2, 3) and location in (20, -20, 2, -2) and self.board[key - (location / 2)] != 0: break
+                    if piece[1] in (0, 1, 2, 3) and location in (11, -11, 9, -9) and captured == 0: break 
+                    yield (piece[2], key, captured)
